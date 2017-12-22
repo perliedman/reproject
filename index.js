a3650d4887d25044f3e7b51bddb222a4b4bfad7d
Per Liedman <per.liedman@dotnetmentor.se>
2017-12-22 10:02:46 +0100

    Refactor

--- index.js
+++ index.js
@@ -23,22 +23,28 @@ function clone(obj) {
   return copy;
 }
 
-function traverseGeoJson(geojson, leafCallback, nodeCallback) {
+function traverseGeoJson(geometryCb, nodeCb, geojson) {
   if (geojson == null) return geojson;
 
   var r = clone(geojson);
+  var self = traverseGeoJson.bind(this, geometryCb, nodeCb);
 
-  if (geojson.type === 'Feature') {
-    r.geometry = traverseGeoJson(geojson.geometry, leafCallback, nodeCallback);
-  } else if (geojson.type === 'FeatureCollection') {
-    r.features = r.features.map(function(gj) { return traverseGeoJson(gj, leafCallback, nodeCallback); });
-  } else if (geojson.type === 'GeometryCollection') {
-    r.geometries = r.geometries.map(function(gj) { return traverseGeoJson(gj, leafCallback, nodeCallback); });
-  } else {
-    if (leafCallback) leafCallback(r);
+  switch (geojson.type) {
+  case 'Feature':    
+    r.geometry = self(geojson.geometry);
+    break;
+  case 'FeatureCollection':
+    r.features = r.features.map(self);
+    break;
+  case 'GeometryCollection':
+    r.geometries = r.geometries.map(self);
+    break;
+  default:
+    geometryCb(r);
+    break;
   }
 
-  if (nodeCallback) nodeCallback(r);
+  if (nodeCb) nodeCb(r);
 
   return r;
 }
@@ -72,6 +78,20 @@ function determineCrs(crs, projs) {
   return crs;
 }
 
+function calcBbox(geojson) {
+  var min = [Number.MAX_VALUE, Number.MAX_VALUE],
+      max = [-Number.MAX_VALUE, -Number.MAX_VALUE];
+  traverseGeoJson(function(_gj) {
+    traverseCoords(_gj.coordinates, function(xy) {
+      min[0] = Math.min(min[0], xy[0]);
+      min[1] = Math.min(min[1], xy[1]);
+      max[0] = Math.max(max[0], xy[0]);
+      max[1] = Math.max(max[1], xy[1]);
+    });
+  }, null, geojson);
+  return [min[0], min[1], max[0], max[1]];
+}
+
 function reproject(geojson, from, to, projs) {
   projs = projs || {};
   if (!from) {
@@ -81,55 +101,40 @@ function reproject(geojson, from, to, projs) {
   }
 
   to = determineCrs(to, projs);
-  var transform = proj4(from, to);
+  var transform = proj4(from, to).forward.bind(transform);
 
-  return traverseGeoJson(geojson, function(gj) {
+  var transformGeometryCoords = function(gj) {
     // No easy way to put correct CRS info into the GeoJSON,
     // and definitely wrong to keep the old, so delete it.
     if (gj.crs) {
       delete gj.crs;
     }
-    gj.coordinates = traverseCoords(gj.coordinates, function(xy) {
-      return transform.forward(xy);
-    });
-  }, function(gj) {
+    gj.coordinates = traverseCoords(gj.coordinates, transform);
+  }
+
+  var transformBbox = function(gj) {
     if (gj.bbox) {
-      // A bbox can't easily be reprojected, just reprojecting
-      // the min/max coords definitely will not work since
-      // the transform is not linear (in the general case).
-      // Workaround is to just re-compute the bbox after the
-      // transform.
-      gj.bbox = (function() {
-        var min = [Number.MAX_VALUE, Number.MAX_VALUE],
-            max = [-Number.MAX_VALUE, -Number.MAX_VALUE];
-        traverseGeoJson(gj, function(_gj) {
-          traverseCoords(_gj.coordinates, function(xy) {
-            min[0] = Math.min(min[0], xy[0]);
-            min[1] = Math.min(min[1], xy[1]);
-            max[0] = Math.max(max[0], xy[0]);
-            max[1] = Math.max(max[1], xy[1]);
-          });
-        });
-        return [min[0], min[1], max[0], max[1]];
-      })();
+      gj.bbox = calcBbox(gj);
     }
-  });
+  }
+
+  return traverseGeoJson(transformGeometryCoords, transformBbox, geojson);
 }
 
 module.exports = {
-    detectCrs: detectCrs,
+  detectCrs: detectCrs,
 
-    reproject: reproject,
+  reproject: reproject,
 
-    reverse: function(geojson) {
-      return traverseGeoJson(geojson, function(gj) {
-        gj.coordinates = traverseCoords(gj.coordinates, function(xy) {
-          return [ xy[1], xy[0] ];
-        });
+  reverse: function(geojson) {
+    return traverseGeoJson(function(gj) {
+      gj.coordinates = traverseCoords(gj.coordinates, function(xy) {
+        return [ xy[1], xy[0] ];
       });
-    },
+    }, null, geojson);
+  },
 
-    toWgs84: function(geojson, from, projs) {
-      return reproject(geojson, from, proj4.WGS84, projs);
-    }
-  };
+  toWgs84: function(geojson, from, projs) {
+    return reproject(geojson, from, proj4.WGS84, projs);
+  }
+};
