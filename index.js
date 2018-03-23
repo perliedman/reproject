3d68da687b6fd30b008388d7de2c23e6b38a3e51
Ralf Junghanns <ralf.junghanns@gmail.com>
2018-03-23 14:08:39 +0100

    Fixes Uncaught TypeError: proj4 is not a function
    Imports proj4.default to prevent Error: Uncaught TypeError: proj4 is not a function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var proj4 = require('proj4');
+var proj4 = require('proj4').default;
 // Checks if `list` looks like a `[x, y]`.
 function isXY(list) {
   return list.length >= 2 &&
